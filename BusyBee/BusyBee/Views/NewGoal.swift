636054827c7edfae8bf688c671a267a9b12406fb
zeptose <mcgrudy1232gmail.com>
2023-10-31 19:38:42 -0400

    new goal file

---
+++ BusyBee/BusyBee/Views/NewGoal.swift
@@ -0,0 +1,18 @@
+//
+//  NewGoal.swift
+//  BusyBee
+//
+//  Created by Ryan McGrady on 10/31/23.
+//
+
+import SwiftUI
+
+struct NewGoal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewGoal()
+}
