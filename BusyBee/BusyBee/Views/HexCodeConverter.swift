19f1180d5957464378c8b101ecb61ff98e26efff
Joshua Yu <xiaokeai@joshuas-mbp-4.wifi.local.cmu.edu>
2023-11-02 14:58:53 -0400

    profile page layout done

---
+++ BusyBee/BusyBee/Views/HexCodeConverter.swift
@@ -0,0 +1,24 @@
+//
+//  HexCodeConverter.swift
+//  BusyBee
+//
+//  Created by Joshua Yu  on 11/2/23.
+//
+
+import SwiftUI
+
+extension UIColor {
+    convenience init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        self.init(red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgb & 0x0000FF) / 255.0,
+                  alpha: 1.0)
+    }
+}
--- BusyBee/BusyBee/Views/ProfileView.swift
+++ BusyBee/BusyBee/Views/ProfileView.swift
@@ -8,13 +8,84 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @EnvironmentObject var viewModel: AuthViewModel
+  let customYellow = Color(UIColor(hex: "#FFD111"))
     var body: some View {
-      Button("Logout") {
-        Task {
-          viewModel.signOut()
+        ZStack {
+          VStack {
+
+            HStack {
+              Button(action: {
+                print("Back button tapped")
+              }) {
+                Image(systemName: "arrow.left")
+              }
+              .padding().padding().padding()
+              
+              Spacer()
+              
+              Button(action: {
+                print("Settings button tapped")
+              }) {
+                Image(systemName: "gear")
+              }
+              .padding()
+            }
+            
+            
+            Image("profilePic")
+              .resizable()
+              .aspectRatio(contentMode: .fill)
+              .frame(width: 150, height: 150)
+              .clipShape(Circle())
+              .overlay(Circle().stroke(customYellow, lineWidth: 10))
+            
+            Text("Your Caption")
+              .font(.headline)
+              .padding()
+            
+            Spacer()
+            
+            HStack {
+              Spacer()
+              
+              Button(action: {
+                print("Current Goals button tapped")
+              }) {
+                Text("Current Goals")
+                  .padding()
+                  .foregroundColor(.white)
+                  .background(Color.yellow)
+                  .cornerRadius(8)
+              }
+              
+              Button(action: {
+                print("Past Goals button tapped")
+              }) {
+                Text("Past Goals")
+                  .padding()
+                  .foregroundColor(.white)
+                  .background(Color.yellow)
+                  .cornerRadius(8)
+              }
+              
+              Spacer()
+            }
+            
+            Spacer()
+          }
         }
-      }
+        .edgesIgnoringSafeArea(.top)
+          // Background Image
+          .background(
+          GeometryReader { geometry in
+            Image("HoneyGraphic")
+              .resizable()
+              .aspectRatio(contentMode: .fill)
+              .frame(width: geometry.size.width)
+              .frame(maxHeight: UIScreen.main.bounds.height / 3)
+              .edgesIgnoringSafeArea(.top)
+          })
+
     }
 }
 
