aa496aecccfe8466a32327fadc684095a72ae86c
Joyce Huang <jhua3662@gmail.com>
2023-11-08 21:04:27 -0500

    Edited filters for past and current goals

--- BusyBee/BusyBee/Controllers/GoalController.swift
+++ BusyBee/BusyBee/Controllers/GoalController.swift
@@ -56,7 +56,7 @@ class GoalController: ObservableObject {
     func getCurrentGoals(currentUser: User) -> [Goal] {
       let usersGoals = self.goals.filter{ String($0.userId) == String(currentUser.id) }
       let today = Date()
-      let curr = usersGoals.filter { $0.progress < $0.frequency && today <= $0.dueDate}
+      let curr = usersGoals.filter { today <= $0.dueDate && $0.progress < $0.frequency }
       return curr.sorted { $0.dueDate >= $1.dueDate}
     }
   
