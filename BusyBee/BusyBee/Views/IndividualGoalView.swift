b2103c4eb50af409c80fab1bff3806cb51452da6
Alexander Ma <alexanderma00@gmail.com>
2023-11-10 06:21:27 -0500

    Individual Goal Page done

--- BusyBee/BusyBee/Views/IndividualGoalView.swift
+++ BusyBee/BusyBee/Views/IndividualGoalView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct IndividualGoalView: View {
     var goal: Goal
     @EnvironmentObject var postController: PostController
+    @EnvironmentObject var goalController: GoalController
+
     @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
@@ -40,7 +42,61 @@ struct IndividualGoalView: View {
                 .padding(.leading, 10)
         }
         .padding()
+        .padding(.bottom, 40)
         .navigationBarHidden(true)
+      
+      if let feedGoal = goalController.getGoalFromId(goalId: goal.id) {
+          let percentage = CGFloat(feedGoal.progress)/CGFloat(feedGoal.frequency)
+        let progressBarMax = UIScreen.main.bounds.width * 0.9
+              RoundedRectangle(cornerRadius: 12)
+                .fill(Color(.clear))
+                .frame(height: 45)
+                .overlay(
+                  VStack{
+                    HStack{
+                      Text("\(feedGoal.progress)/\(feedGoal.frequency)")
+                        .font(.system(size: 18))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 25)
+
+                    }
+                    ZStack(alignment: .leading) {
+                      Capsule().frame(width: progressBarMax)
+                        .foregroundColor(Color.gray)
+                      Capsule().frame(width: progressBarMax * percentage)
+                        .foregroundColor(Color.yellow)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .frame(height: 20)
+                    .padding(.horizontal, 25)
+                    .padding(.bottom, 40)
+                  }
+                )
+                
+        }
+      
+        VStack (alignment: .leading) {
+          //Subgoal
+            ForEach(goalController.getSubgoalsForGoal(goal: goal)) { subgoal in
+              Capsule()
+                .foregroundColor(Color.green)
+                .frame(height: 30, alignment: .leading)
+                .overlay(
+                  HStack{
+                    Image(systemName: "checkmark.circle")
+                      .frame(width: 20, alignment: .leading)
+                      .padding(.leading, 8)
+                    Text(subgoal.name)
+                      .font(.system(size: 10))
+                      .frame(maxWidth: .infinity, alignment: .leading)
+                  }
+                )
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 25)
+
+            }
+        }.padding(.bottom, 30)
+          
 
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 15) {
