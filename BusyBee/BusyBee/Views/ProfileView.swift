7766c4a19d03fe305cee8f1f762f47b6a6343b6d
Joyce Huang <jhua3662@gmail.com>
2023-12-05 20:23:03 -0500

    Edited completed goals tab

--- BusyBee/BusyBee/Views/ProfileView.swift
+++ BusyBee/BusyBee/Views/ProfileView.swift
@@ -166,38 +166,10 @@ struct ProfileView: View {
                                         
                                         ForEach(pastGoals) { goal in
                                             NavigationLink(destination: IndividualGoalView(goal: goal)) {
-                                                RoundedRectangle(cornerRadius: 12)
-                                                    .fill(Color.white)
-                                                    .shadow(color: Color.gray, radius: 4, x: 0, y: 2)
-                                                    .frame(width: UIScreen.main.bounds.width * 0.85, height: 120)
-                                                    .overlay(
-                                                      VStack(alignment: .leading) {
-                                                          HStack {
-                                                              Text(goal.name)
-                                                                  .font(.title)
-                                                                  .foregroundColor(Color.black)
-                                                                  .fontWeight(.bold)
-                                                                  .padding(.bottom, 4)
-                                                              Spacer()
-                                                              NavigationLink(destination: EditGoalView(goalController: goalController, currentUser: updatedUser!, goal: goal)) {
-                                                                  Image(systemName: "pencil")
-                                                                      .foregroundColor(.black)
-                                                                      .font(.title)
-                                                              }
-                                                          }
-                                                          HStack {
-                                                              Text("Due: \(dateFormatter.string(from: goal.dueDate))")
-                                                                  .font(.subheadline)
-                                                                  .foregroundColor(Color.black)
-                                                              Spacer()
-                                                              Text("Frequency: \(goal.frequency)")
-                                                                  .font(.subheadline)
-                                                                  .foregroundColor(Color.black)
-                                                                  .multilineTextAlignment(.trailing)
-                                                          }
-                                                      }
-                                                            .padding(12)
-                                                    )
+                                                
+                                                NavigationLink(destination: IndividualGoalView(goal: goal)) {
+                                                    GoalCardView(user: updatedUser!, goal: goal)
+                                                }
                                             }
                                         }
                                     }
