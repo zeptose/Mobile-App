b1a630c4caa0edbdb0d0366b0f6207e804a2bc34
zeptose <mcgrudy1232gmail.com>
2023-12-07 16:46:38 -0500

    xdddd

--- BusyBee/BusyBee/Views/NotificationsView.swift
+++ BusyBee/BusyBee/Views/NotificationsView.swift
@@ -67,7 +67,7 @@ struct NotificationView: View {
       if let currentUser = viewModel.currentUser {
         notifications = postController.getNotificationsForCurrentUser(currentUser: currentUser)
         notifications.sort()
-        
+         
       }
     }
     
